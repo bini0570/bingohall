6a21cbfef44ef013321dbe943eb7531ba1d96532
Biniyam Eyoel <eyoelbiniyam876@gmail.com>
2026-09-18 16:19:06 +0300

    Convert admin panel into a Progressive Web App (PWA) for native mobile installation

---
+++ admin/public/sw.js
@@ -0,0 +1,14 @@
+﻿self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Simple pass-through fetch to satisfy Chrome PWA install requirements
+  e.respondWith(
+    fetch(e.request).catch(() => new Response('Offline mode not supported.'))
+  );
+});
